6c9fe11b7fed50349418b3d2e007c6ec9253da15
hpy777 <iosios1223@163.com>
2019-03-25 16:41:42 +0800

    no message

--- tomlconfig/parsePubk_test.go
+++
@@ -1,7 +0,0 @@
-package tomlconfig
-
-/*func TestName(t *testing.T) {
-	config := TomlDefaultConfig()
-	 privVal := privval.LoadFilePV(config.PrivValidatorKeyFile(), config.PrivValidatorStateFile())
-     fmt.Println(string(privVal.GetPubKey().Bytes()))
-}*/
